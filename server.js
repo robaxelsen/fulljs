bedaf8634b5a015f7f7cb5625882a7451a26cc8f
robeerob <rob@rob.ee>
2017-03-11 20:38:24 +0100

    Adding EJS.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import config from './config';
+import apiRouter from './api';
 
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', {
+    content: 'Hello Express and EJS!'
+  });
 });
 
+server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
