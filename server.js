94923bce5d5a15686c64cb890db0c6ffdf3a1fb5
robeerob <rob@rob.ee>
2017-03-11 20:19:47 +0100

    Adjust package.json vs numbers to be in line with course file, implement express, and add html files.

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-import http from 'http';
+import config from './config';
 
-const server = http.createServer();
+import express from 'express';
+const server = express();
 
-server.on('request', (req, res) => {
-  res.write('Hello HTTP!\n');
-  setTimeout(() => {
-    res.write('I can stream!\n');
-    res.end();
-  }, 3000);
+server.get('/', (req, res) => {
+  res.send('Hello Express');
 });
 
-server.listen(8080);
+server.use(express.static('public'));
 
-//import https from 'https';
-//https.get('https://www.lynda.com', res => {
-//  console.log('Response status code: ', res.statusCode);
-//  res.on('data', chunk => {
-//    console.log(chunk.toString());
-//  });
-//});
+server.listen(config.port, () => {
+  console.log('Express listening on port ', config.port);
+});
