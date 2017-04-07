e6704504926726507ebcf3c0529e68095d0f893e
robeerob <rob@rob.ee>
2017-04-07 18:03:53 +0200

    Import and add ReactDOMServer

--- serverRender.js
+++ serverRender.js
@@ -1,9 +1,13 @@
 // TODO: Fetch the data from the api
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from './src/components/App';
 
 import config from './config';
 import axios from 'axios';
 
 axios.get(`${config.serverUrl}/api/contests`)
   .then(response => {
-    console.log(response.data);
+    ReactDOMServer.renderToString(<App />);
   });
