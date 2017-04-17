0f802c76c70eba7296447dc8c76edfd6b813d7b7
robeerob <rob@rob.ee>
2017-04-17 10:11:19 +0200

    Add api.js to App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 import Contest from './Contest';
+import * as api from '../api';
 
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
@@ -19,15 +20,19 @@ class App extends React.Component {
   }
   fetchContest = (contestId) => {
     pushState(
-      { currenContestId: contestId },
+      { currentContestId: contestId },
       `/contest/${contestId}`
     );
-    // lookup the state
-    // this.state.contest[contestId]
-    this.setState({
-      pageHeader: this.state.contests[contestId].contestName,
-      currentContestId: contestId
-    })
+    api.fetchContest(contestId).then(contest => {
+      this.setState({
+        pageHeader: contest.contestName,
+        currentContestId: contest.id,
+        contests: {
+          ...this.state.contests,
+          [contest.id]: contest
+        }
+      });
+    });
   };
   currentContent() {
     if (this.state.currentContestId) {
