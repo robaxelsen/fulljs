1937a3c57196edcf262d0f5fd4663ac35cd214f3
robeerob <rob@rob.ee>
2017-04-17 10:02:13 +0200

    Add id specific API endpoints

--- api/index.js
+++ api/index.js
@@ -7,10 +7,16 @@ const contests = data.contests.reduce((obj, contest) => {
   return obj;
 }, {})
  
-router.get('/contests', (req, res) => {
-  res.send({
+router.get('/contests', (request, response) => {
+  response.send({
     contests: contests
   });
+});
+
+router.get('/contests/:contestId', (request, response) => {
+  let contest = contests[request.params.contestId];
+  contest.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus augue a massa euismod, ac efficitur urna pulvinar. Integer pretium tempus viverra. Proin nec urna sit amet est facilisis tristique et hendrerit odio. Nam ut neque sagittis, laoreet ante id, tincidunt mi. Nunc vel orci nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas scelerisque sem ac bibendum volutpat. Maecenas tincidunt sagittis malesuada. Suspendisse ullamcorper massa sed ante sodales faucibus. Duis et urna mollis, elementum quam a, pulvinar lacus.'
+  response.send(contest);
 });
 
 export default router;
